0ed30e5ad6f32ace9a697c0368b8199f4a0c6f80
Rendraa23 <rendraarka805@gmail.com>
2023-11-13 09:13:35 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "jobsheet11"
+include(":app")
